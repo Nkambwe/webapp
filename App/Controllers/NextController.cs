304c019e26d4d20863d3deb8ab01bb13e5fb5820
@mark.nkambwe <mark.nkambwe@postbank.co.ug>
2024-05-20 09:21:12 +0300

    Initial commit

---
+++ App/Controllers/NextController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers {
+    public class NextController : Controller {
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult LoadView(string partialName) {
+            return new PartialViewResult {
+                ViewName = partialName
+            };
+        }
+
+        public IActionResult LoadPageView(string partialName) {
+            return new PartialViewResult {
+                ViewName = partialName
+            };
+        }
+    }
+}
---
+++ App/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    $(".nav-item-button").click(function () {
+
+        const partialName = $(this).data('partial');
+        $('.page-container').load('/Next/LoadView?partialName=' + partialName);
+
+        //..display overlay
+        on();
+
+        //..slide in page
+        $('.page-container').addClass('show-page');
+
+    });
+
+});
+
+function on(){
+    $("#overlay").css('display', 'block');
+};
+
+
+
+
